e9d5609684d55c4f38f19ed33ce15c487417889e
Michael Ovdiienko <mishaov@gmail.com>
2022-02-08 18:35:25 +0200

    feat: separated js files for scripts

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-$(function (){
-
-    $(".head_inner nav ul li.switch a").on("click", function(e){
-       $("body").toggleClass("switchMode");
-        let src = ($("#my_image").attr("src") === "img/logo.png")
-            ? "img/logo_white.png"
-            : "img/logo.png";
-        $("#my_image").attr("src", src);
-       e.preventDefault();
-    });
-
-});
-
 let header__burger = document.querySelector('.header__burger');
 let header_menu = document.querySelector('.header__menu');
 let back = document.querySelector('body');
@@ -29,22 +16,3 @@ header__link.forEach(function (item) {
         back.classList.toggle('lock');
     }
 });
-
-
-$(document).ready(function(){
-
-    let headerHeight = $('.header_head').outerHeight();
-
-    $('.header__link').click(function(e){
-
-        let linkHref = $(this).attr('href');
-
-        $('html,body').animate({
-            scrollTop: $(linkHref).offset().top - headerHeight
-        }, 1000);
-
-        e.preventDefault();
-
-    })
-
-})
